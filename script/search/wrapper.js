9e06d40eaf7c56cfce7f9ced8394955530898bb2
Moghedrin <nolat301@gmail.com>
2013-09-03 14:42:08 -0600

    Added wrapper for Queue and PriorityQueue

---
+++ script/search/wrapper.js
@@ -0,0 +1,10 @@
+function Queue() {}
+Queue.prototype = new buckets.Queue();
+Queue.prototype.push = Queue.prototype.enqueue;
+Queue.prototype.pop = Queue.prototype.dequeue;
+
+function PriorityQueue(compare) {
+	this.__proto__ = new buckets.PriorityQueue(compare);
+}
+PriorityQueue.prototype.push = Queue.prototype.enqueue;
+PriorityQueue.prototype.pop = Queue.prototype.dequeue;
